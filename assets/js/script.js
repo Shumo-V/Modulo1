6557af602bef7a15498b691ba463e0f04061ca68
Víctor Valenzuela <shumolxs@gmail.com>
2021-12-19 17:57:47 -0400

    Favicon y Tooltip agregados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
 $(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-
-    $('#enviarCorreo').click(function() {
-        alert('El correo fue enviado correctamente...');
-    });
-
-    $('body').on('dblclick', 'u', function() {
-        $(this).css('color', 'red');
-    });
-
-    $('.card-title').click(function() {
-        $('.card-text').toggle();
-    });
-});
+    $('[data-toggle="tooltip"]').tooltip()
+})
