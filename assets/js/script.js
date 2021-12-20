b93fe68b7a4d6dfcf186a50f1d89de4763cbff30
Víctor Valenzuela <shumolxs@gmail.com>
2021-12-19 20:52:06 -0400

    Scrollspy y Smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-$(function() {
+$(document).ready(function() {
+
+    // Añade el comportamiento smooth a todos los enlaces
+
+    $("a").click(function() {
+        var gato = this.hash
+
+        $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            },
+            800
+        )
+    });
+
     $('[data-toggle="tooltip"]').tooltip()
 })
